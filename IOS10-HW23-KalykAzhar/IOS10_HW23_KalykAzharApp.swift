1fe0c1778349ebc8ab52e01afbc08d6105cfdcf0
AzharaKalyk <kab_180495@mail.ru>
2023-09-30 22:24:57 +0600

    Initial Commit

---
+++ IOS10-HW23-KalykAzhar/IOS10_HW23_KalykAzharApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOS10_HW23_KalykAzharApp.swift
+//  IOS10-HW23-KalykAzhar
+//
+//  Created by Azhar Kalyk on 30.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct IOS10_HW23_KalykAzharApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
