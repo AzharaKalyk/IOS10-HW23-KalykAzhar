e817fe8059fd07fdea95a7d54227b735dde42a1b
AzharaKalyk <kab_180495@mail.ru>
2023-10-21 23:51:11 +0600

    create_home_work24

---
+++ IOS10-HW23-KalykAzhar/Views/RadioView/View/RadioView.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+struct RadioView: View {
+    private var models = ModelRadio()
+        
+    var rows = [GridItem()]
+        
+    var body: some View {
+        ScrollView(.horizontal, showsIndicators: false) {
+            LazyHGrid(rows: rows) {
+                ForEach(models.models, id: \.id) { model in
+                    VStack(alignment: .leading) {
+                        Divider()
+                        Text(model.top)
+                            .foregroundColor(.secondary)
+                        Text(model.middle)
+                            .font(.title2)
+                        Text(model.bottom)
+                            .foregroundColor(.secondary)
+                        Image(model.image)
+                            .resizable()
+                            .frame(width: 350, height: 200)
+                            .cornerRadius(20)
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct RadioView_Previews: PreviewProvider {
+    static var previews: some View {
+        RadioView()
+    }
+}
+
